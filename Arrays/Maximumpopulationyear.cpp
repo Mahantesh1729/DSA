25478afb37402ffa73ac78f9711cdfdaa40a2869
Mahantesh Gattina <67777625+Mahantesh1729@users.noreply.github.com>
2021-09-16 07:46:40 +0530

    approach1

---
+++ Arrays/Maximumpopulationyear.cpp
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/maximum-population-year/
+//Approach1
+class Solution {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+       map <int, int> mp;
+        
+        for(int i = 1950; i <= 2050; i++)
+        {
+            mp[i] = 0;
+        }
+        
+        int n = logs.size();
+        
+        for(int i = 1950; i <= 2050; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(logs[j][0] <= i && i < logs[j][1])
+                {
+                    mp[i]++;
+                }
+            }
+        }
+        int year, mx = 0; 
+        for(auto i: mp)
+        {
+            if(i.second > mx)
+            {
+                mx = i.second;
+                year = i.first;
+            }
+        }
+        return year;
+        
+    }
+};
+//approach2
