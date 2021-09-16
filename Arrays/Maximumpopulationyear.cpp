24bc0e1dc85f4eb5564fca3c5a22acc1ad8cebc7
Mahantesh Gattina <67777625+Mahantesh1729@users.noreply.github.com>
2021-09-16 08:05:31 +0530

    Update Maximumpopulationyear.cpp

--- Arrays/Maximumpopulationyear.cpp
+++ Arrays/Maximumpopulationyear.cpp
@@ -36,3 +36,42 @@ public:
     }
 };
 //approach2
+class Solution {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+        int minYear = INT_MAX;
+        int maxPop = INT_MIN;
+        int year;
+        int pop, prev = 0;
+        int n = logs.size();
+        
+        
+        for(int i = 0; i < n; i++)
+        {
+            year = logs[i][0];
+            pop = 1;
+            for(int j = 0; j < n; j++)
+            {
+                if(logs[j][0] <= year && year < logs[j][1])
+                {
+                    pop++;
+                }
+            }
+            if(pop > maxPop)
+            {
+                maxPop = pop;
+                minYear = year;
+            }
+            else if(pop == maxPop)
+            {
+                if(year < minYear)
+                {
+                    minYear = year;
+                }
+            }
+        }
+        
+        return minYear;
+        
+    }
+};
