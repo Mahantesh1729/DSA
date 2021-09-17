cf482b800df71376702c3e8e150276a26035f3ad
Mahantesh Gattina <67777625+Mahantesh1729@users.noreply.github.com>
2021-09-17 09:05:02 +0530

    Create setMatrixZero.cpp

---
+++ Arrays/setMatrixZero.cpp
@@ -0,0 +1,29 @@
+// Approach1
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<vector<int>> temp = matrix;
+        int m = matrix.size();
+        int  n = matrix[0].size();
+        
+        
+        
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+            {
+                if(temp[i][j] == 0)
+                {
+                    for(int k = 0; k < m; k++)
+                    {
+                        matrix[k][j] = 0;
+                    }
+                    
+                    for(int k = 0; k < n; k++)
+                    {
+                        matrix[i][k] = 0;
+                    }
+                }
+            }
+    }
+};
