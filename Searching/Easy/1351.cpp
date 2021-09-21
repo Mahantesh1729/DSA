8aabc926d54704317bc40265bf8ef559b4e82196
Mahantesh Gattina <67777625+Mahantesh1729@users.noreply.github.com>
2021-09-21 15:13:22 +0530

    Create 1351.cpp

---
+++ Searching/Easy/1351.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+// Approach 1
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& arr) {
+        int m = arr.size(), sum = 0;
+        for(int i = 0; i < m; i++)
+       {
+              sum += search(arr[i], -1);
+       }
+
+       return sum;
+        
+    }
+    int search(vector <int> arr, int k)
+{
+       int n = arr.size();
+        if(arr[0] <= -1)
+        {
+            return n;
+        }
+       int h = n - 1, l = 0, mid, ans = 0;
+
+       while(h >= l)
+       {
+              mid = (h + l) / 2;
+              if(arr[mid] <= -1)
+              {
+                     ans = n - mid;
+                     h = mid - 1;
+              }
+              else
+              {
+                     l = mid + 1;
+              }
+       }
+        if(arr[mid] > -1) mid++;
+        if (!ans)
+              return 0;
+       else
+              return n - mid;
+       return 0;
+}
+};
